e14fa80be879dcb5a314b565aa07929071fe387c
Kenneth Korcal <kakorcal@gmail.com>
2016-08-02 03:52:39 -0700

    finish simple example for now

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ if(module.hot) module.hot.accept();
 
 require('./index.html');
 require('./styles.css');
+
+// generates random color
+var Please = require('pleasejs');
+var title = document.getElementById('title');
+var color = document.getElementById('color');
+var button = document.getElementById('colorButton');
+
+color.innerText = window.getComputedStyle(title, null).color;
+
+button.addEventListener('click', changeColor);
+
+function changeColor(){
+  title.style.color = Please.make_color();
+  color.innerText = title.style.color;
+}
